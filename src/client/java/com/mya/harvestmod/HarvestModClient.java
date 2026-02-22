da68a32d4fc009b4cf05299b6efb52da020096ff
Mya Schmitz <myaschm@gmail.com>
2026-02-22 00:21:03 -0800

    initial commit: fabric mod template

---
+++ src/client/java/com/mya/harvestmod/HarvestModClient.java
@@ -0,0 +1,10 @@
+package com.mya.harvestmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HarvestModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
